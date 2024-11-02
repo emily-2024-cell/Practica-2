e78331ce47b1aede4835ae67eac6b0a355bd0b8f
Elthon Pessoa Piña <elthon.pessoa30@gmail.com>
2024-11-01 18:45:08 -0600

    Parte 2 de mis archivos del curso
    Unos cuantos archivos mas del curso de programacion

---
+++ Clase2 Thonny.py
@@ -0,0 +1,16 @@
+#condiciones}
+#***
+#= = igualdad
+#i = diferencia
+#> = mayor
+#< = menor
+#>= = mayor o igual
+#<= = menor o igual
+
+nombre = input("Digite su nombre")
+
+
+if("steph"== nombre):
+    print("El nombre es igual")
+else:
+    print("el nombre noes igual")
